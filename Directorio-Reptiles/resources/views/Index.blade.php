850626a3b2dccbb886870c9d63c67446d4d136e4
Nokturne <krothaloinf@gmail.com>
2018-12-14 15:45:36 -0700

    starting up

---
+++ Directorio-Reptiles/resources/views/Index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <title></title>
+  </head>
+  <body>
+    @include('layouts.nav')
+  </body>
+</html>
---
+++ Directorio-Reptiles/resources/views/layouts/nav.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <div class="container-fluid">
+         <div class="navbar-header">
+            <button type="button" class="collapsed navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-9" aria-expanded="false">
+               <span class="sr-only">Toggle navigation</span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+            </button>
+             <a href="#" class="navbar-brand">Brand</a>
+            </div> <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-9">
+               <ul class="nav navbar-nav">
+                  <li class="active"><a href="#">Home</a></li>
+                  <li><a href="#">Link</a></li>
+                  <li><a href="#">Link</a></li>
+               </ul>
+              </div>
+      </div>
+  </body>
+</html>
